1313b0b6f01035f9a8ad35eb23e3ce6d7d9e7645
Tom Shlomo <tomshlomo@gmail.com>
2020-10-18 16:36:57 +0300

    numerically stable version of shmat

--- spherical processing/shmat.m
+++ spherical processing/shmat.m
@@ -1,110 +1,43 @@
-function Y_N = shmat(N, dirs, isComplex, transposeFlag)
-%GETSH Get Spherical harmonics up to order N
-%
-%   N:  maximum order of harmonics
-%   dirs:   [azimuth_1 inclination_1; ...; azimuth_K inclination_K] angles
-%           in rads for each evaluation point, where inclination is the
-%           polar angle from zenith: inclination = pi/2-elevation
-%   basisType:  'complex' or 'real' spherical harmonics (default:
-%   'complex')
-%   transposeFlag
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%   Archontis Politis, 10/10/2013
-%   archontis.politis@aalto.fi
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% Modified by Tom Shlomo, Dec 2019
-% - Removed last transposition that was quite expensive.
-% - Added option for transposed output (useful for steering matrices)
-% - Added defaults
-% - changes dirs to be [theta phi], where theta is angle with the z-axis
-%   (top to bottom).
-
-if nargin<3
-    isComplex = true;
-end
-if nargin<4 || isempty(transposeFlag)
-    transposeFlag = false;
+function Y = shmat(N, omega, is_complex, transpose_flag)
+% Author: Tom Shlomo, 2020
+arguments
+    N (1,1) double
+    omega (:,2) double % should be [theta, phi], both in radians.
+    is_complex (1,1) logical = true
+    transpose_flag (1,1) logical = false
 end
 
-Ndirs = size(dirs, 1);
-Nharm = (N+1)^2;
+assert(is_complex, "Sorry, real SH are not implemented (yet)");
+G = size(omega, 1);
+Q = (N+1)^2;
 
-if ~transposeFlag
-    Y_N = zeros(Ndirs, Nharm);
+cos_th = cos(omega(:,1));
+if ~transpose_flag
+    Y = zeros(G, Q);
+    phase_m_positive = [ones(G, 1), exp(1i * (1 : N) .* omega(:, 2))] / sqrt(2 * pi);
+    % the sqrt(2*pi) is due to our normalization stadard.
+    phase_m_negative = conj(phase_m_positive(:, end : -1 : 2));
+%     phase_m_negative(1:2:end) = -phase_m_negative(1:2:end);
+    phase_m_positive(:, 2 : 2 : end) = -phase_m_positive(:, 2 : 2 : end);
 else
-    Y_N = zeros(Nharm, Ndirs);
+    Y = zeros(Q, G);
+    phase_m_positive = [ones(1, G); exp(1i * (1 : N).' .* omega(:, 2).')] / sqrt(2 * pi);
+    phase_m_negative = conj(phase_m_positive(end : -1 : 2, :));
+    phase_m_positive(2 : 2 : end, :) = -phase_m_positive(2 : 2 : end, :);
 end
-idx_Y = 0;
+[n_vec, ~] = i2nm(1:Q);
+
 for n=0:N
-    m = (0:n)';
-    if isComplex
-        % vector of unnormalised associated Legendre functions of current order
-        Lnm = legendre(n, cos(dirs(:,1)'));
-        
-        % normalisations
-        norm = sqrt( (2*n+1)*factorial(n-m) ./ (4*pi*factorial(n+m)) );
-        
-        % convert to matrix, for direct matrix multiplication with the rest
-        Nnm = norm * ones(1,Ndirs);
-        
-        % spherical harmonics of current order
-        Exp = exp(1i*m*dirs(:,2)');
-        Ynm_pos = Nnm .* Lnm .* Exp;
-        
-        if n~=0
-            % conjugate relation Y_{n(-m)} = (-1)^m (Y_{nm})^* for negative
-            % degrees
-            condon = (-1).^m(end:-1:2) * ones(1,Ndirs);
-            Ynm_neg = condon .* conj(Ynm_pos(end:-1:2,:));
-        else
-            Ynm_neg = [];
-        end
-        
-        % final SHs
-        Ynm = [Ynm_neg; Ynm_pos];
+    v = legendre(n, cos_th, "norm"); % n+1 x G
+    v = v([end : -1 : 1, 2 : end], :);
+    if ~transpose_flag
+        v = v.'; % G x n+1
+        phase = [phase_m_negative(:, end - n + 1 : end), phase_m_positive(:, 1 : n + 1)];
+        Y(:, n_vec == n) = v .* phase;
     else
-        warning("I am not sure this definition of the real spherical harmonics is the same one used in kennedy's book. there is a good chance that a (-1)^m is missing");
-        % vector of unnormalised associated Legendre functions of current order
-        Lnm_real = legendre(n, cos(dirs(:,1)'));
-        if n~=0
-            % cancel the Condon-Shortley phase from the definition of
-            % the Legendre functions to result in signless real SH
-            condon = (-1).^[m(end:-1:2);m] * ones(1,Ndirs);
-            Lnm_real = condon .* [Lnm_real(end:-1:2, :); Lnm_real];
-        end
-        
-        % normalisations
-        norm_real = sqrt( (2*n+1)*factorial(n-m) ./ (4*pi*factorial(n+m)) );
-        
-        % convert to matrix, for direct matrix multiplication with the rest
-        Nnm_real = norm_real * ones(1,Ndirs);
-        if n~=0
-            Nnm_real = [Nnm_real(end:-1:2, :); Nnm_real];
-        end
-        
-        CosSin = zeros(2*n+1,Ndirs);
-        % zero degree
-        CosSin(n+1,:) = ones(1,size(dirs,1));
-        % positive and negative degrees
-        if n~=0
-            CosSin(m(2:end)+n+1,:) = sqrt(2)*cos(m(2:end)*dirs(:,2)');
-            CosSin(-m(end:-1:2)+n+1,:) = sqrt(2)*sin(m(end:-1:2)*dirs(:,2)');
-        end
-        Ynm = Nnm_real .* Lnm_real .* CosSin;
-        
+        phase = [phase_m_negative(end - n + 1 : end, :); phase_m_positive(1 : n + 1, :)];
+        Y(n_vec == n, :) = v .* phase;
     end
-    
-    % Ynm is now degreea x direcions
-    
-    if transposeFlag
-        Y_N(idx_Y+1:idx_Y+(2*n+1), :) = Ynm; % sh x directions
-    else
-        Y_N(:, idx_Y+1:idx_Y+(2*n+1)) = Ynm.'; % direcions x sh
-    end
-    idx_Y = idx_Y + 2*n+1;
 end
 
 
