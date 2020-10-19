3a615fe12e65e814e3757a6354b1bf864de61369
Tom Shlomo <tomshlomo@gmail.com>
2020-10-19 21:49:34 +0300

    Update setup.m

--- setup.m
+++ setup.m
@@ -6,6 +6,12 @@ function setup()
 set(0,'defaulttextinterpreter','latex');  
 set(0, 'defaultAxesTickLabelInterpreter','latex');  
 set(0, 'defaultLegendInterpreter','latex');
+set(0, 'defaultAxesXGrid', 'on')
+set(0, 'defaultAxesYGrid', 'on')
+set(0, 'defaultAxesZGrid', 'on')
+set(groot,'defaultAxesXMinorGrid','off','defaultAxesXMinorGridMode','manual');
+set(groot,'defaultAxesYMinorGrid','off','defaultAxesYMinorGridMode','manual');
+set(groot,'defaultAxesZMinorGrid','off','defaultAxesZMinorGridMode','manual');
 
 %% path
 restoredefaultpath();
